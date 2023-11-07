95ca82b5861468781ca51d50dd80f4c92268150e
clintmadeit <clintonbrirdley@gmail.com>
2023-11-07 15:40:48 +0300

    update server running message

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ mongoose.connect(process.env.MONGO).then(() => {
 const app = express();
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port 3000!");
 });
 
 app.use("/api/user", userRouter);
